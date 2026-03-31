cfac8399d801cdc9229f4035f43c7c8fdd6749ca
wxwzch <66943137+wxwzch@users.noreply.github.com>
2026-03-31 23:49:47 +0800

    Update bootimg.mk

--- bootimg.mk
+++ bootimg.mk
@@ -1,8 +1,14 @@
-# MTK specific boot image configuration
+# MTK specific boot image creation
 LOCAL_PATH := $(call my-dir)
 
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
-
-define build_bootimg
-$(call build-mtk-bootimg, $(1))
-endef
+ifeq ($(TARGET_DEVICE),KT961)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_FILES)
+	$(call pretty,"Target recovery image: $@")
+	$(MKBOOTIMG) --kernel $(TARGET_PREBUILT_KERNEL) --ramdisk $(recovery_ramdisk) \
+		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
+		--base $(BOARD_KERNEL_BASE) \
+		--pagesize $(BOARD_KERNEL_PAGESIZE) \
+		$(BOARD_MKBOOTIMG_ARGS) \
+		--output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
+endif
